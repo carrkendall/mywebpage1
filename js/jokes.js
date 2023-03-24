7ba032e5b7e79bb1cb5f535cdc8327c82082c9bf
Kendall Carr <111819750+carrkendall@users.noreply.github.com>
2023-03-24 05:43:14 +0000

    Fri, Mar 24, 2023, 5:43 AM +00:00

--- js/jokes.js
+++ js/jokes.js
@@ -4,10 +4,10 @@ const jokeBtn = document.getElementbyId('jokeBtn')
 jokeBtn.addEventListener('click', generateJoke)
 
 async function generateJoke(){
-    const res = await fetch('https://icanhazdadjoke.com', {
-        headers: {Accept: 'application/json',},
+    const response = await fetch('https://icanhazdadjoke.com', {
+        headers: {Accept: 'application/json'},
     })
 
-    const data = await res.json()
+    const data = await response.json()
     jokeEl.innerHTML = data.joke
 }
