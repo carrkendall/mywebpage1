d4b7b62feb4cfd854f54243b17bb047cf4cfca04
Kendall Carr <111819750+carrkendall@users.noreply.github.com>
2023-03-27 23:21:18 +0000

    Mon, Mar 27, 2023, 11:21 PM +00:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -11,6 +11,10 @@ for(let i = 0; i < SQUARES; i++) {
 }
 
 
+function removeColor(element) {
+    element.style.background = '#1d1d1d'
+}
+
 function setColor(element) {
     const color = getRandomColor()
     element.style.background = color
