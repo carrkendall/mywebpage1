995d646bed2287336847e93490017eaec4a8e781
Kendall Carr <111819750+carrkendall@users.noreply.github.com>
2023-03-28 03:24:24 +0000

    Tue, Mar 28, 2023, 3:24 AM +00:00

--- js/typing.js
+++ js/typing.js
@@ -9,11 +9,13 @@ writeText()
 function writeText() {
     textEl.innerText = text.slice(0,idx)
     idx++
-
     if (idx > text.length) {
         idx = 1
     }
-
     setTimeout(writeText, speed)
 }
+
+speedEl.addEventListener('input', (e) => {
+    speed = 300 / e.target.value
+})
 
