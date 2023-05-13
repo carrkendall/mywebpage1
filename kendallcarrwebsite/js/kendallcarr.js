99ae99136bc52a9c1122f4f1393850c84b27f48a
Kendall Carr <111819750+carrkendall@users.noreply.github.com>
2023-05-13 02:16:36 +0000

    Sat, May 13, 2023, 2:16 AM +00:00

---
+++ kendallcarrwebsite/js/kendallcarr.js
@@ -0,0 +1,8 @@
+var button = document.getElementById("button");
+
+// Add a click event listener
+button.addEventListener("click", function() {
+  // Change the background color
+  button.style.backgroundColor = "red";
+});
+
