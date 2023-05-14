0ef1ca6343f9e4c44923928418c1a7b1429c763b
Kendall Carr <111819750+carrkendall@users.noreply.github.com>
2023-05-14 01:21:57 +0000

    Sun, May 14, 2023, 1:21 AM +00:00

--- kendallcarrwebsite/js/kendallcarr.js
+++ kendallcarrwebsite/js/kendallcarr.js
@@ -1,8 +1,9 @@
 <script src="kendallcarr.js"></script>
 var button = document.getElementById("button");
-
+const myDiv = document.getElementById('links');
 // Add a click event listener
 button.addEventListener("click", function() {
   // Change the background color
   button.style.backgroundColor = "red";
+  myDiv.style.backgroundColor = ‘green’;
 });
